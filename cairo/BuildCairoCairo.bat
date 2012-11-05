a78b01996619286d5b0bde4de2ee8be4152e77cc
teknos293 <teknos293@2ff3f8d9-40b9-4790-838c-7678beded962>
2012-11-05 14:41:37 +0000

    Cleaning up old cairo and pixman versions & ensure they compile.

--- cairo/BuildCairoCairo.bat
+++ cairo/BuildCairoCairo.bat
@@ -5,9 +5,9 @@ set ROOTDIR=%CD%\..
 
 set INCLUDE=%INCLUDE%;%ROOTDIR%\zlib
 set INCLUDE=%INCLUDE%;%ROOTDIR%\libpng
-set INCLUDE=%INCLUDE%;%ROOTDIR%\pixman-0.26.0\pixman
-set INCLUDE=%INCLUDE%;%ROOTDIR%\cairo-1.12.6\boilerplate
-set INCLUDE=%INCLUDE%;%ROOTDIR%\cairo-1.12.6\cairo\src
+set INCLUDE=%INCLUDE%;%ROOTDIR%\pixman\pixman
+set INCLUDE=%INCLUDE%;%ROOTDIR%\cairo\boilerplate
+set INCLUDE=%INCLUDE%;%ROOTDIR%\cairo\cairo\src
 
 set LIB_SAVE=%LIB%
 
