37b4a1758615b3bc947eef2c649d721e5d1c882b
teknos293 <teknos293@2ff3f8d9-40b9-4790-838c-7678beded962>
2014-02-26 19:13:50 +0000

    Expand now is on a better track...

--- trunk/libmscgen/contour_simple.h
+++ trunk/libmscgen/contour_simple.h
@@ -279,19 +279,18 @@ class SimpleContour
     {
         const int           original_edge;
         Edge::EExpandCPType cross_type; //The type of crossing between edge #i and #i+1 (wrapped around)
-        XY                  original_point; // The original end vertex (for creating the round circle)
         XY                  cross_point;//The cp between edge #i and #i+1 (wrapped around)
         XY                  prev_tangent; //A point along the linear continuation of this edge's start. Used only for CP_PARALLEL and CP_EXTENDED
         XY                  next_tangent; //A point along the linear continuation of this edge's end. Used only for CP_PARALLEL and CP_EXTENDED
         double              us_end_pos;
         double              next_start_pos;
 
-        ExpandMetaData(const Edge &e, int o=-1, Edge::EExpandCPType a = Edge::CP_TRIVIAL, const XY &b = XY(), const XY &c = XY(), double ue=double(), double ns=double()) :
-            Edge(e), original_edge(o), cross_type(a), original_point(b), cross_point(c), us_end_pos(ue), next_start_pos(ns) {}
-        ExpandMetaData(const XY&s, const XY &d, bool v = true, Edge::EExpandCPType a = Edge::CP_TRIVIAL, const XY &b = XY(), const XY &c = XY(), double ue = double(), double ns = double()) :
-            Edge(s, d, v), original_edge(-1), cross_type(a), original_point(b), cross_point(c), us_end_pos(ue), next_start_pos(ns) {}
-        ExpandMetaData(const XY&s, const XY &d, const XY&c1, const XY &c2, bool v = true, Edge::EExpandCPType a = Edge::CP_TRIVIAL, const XY &b = XY(), const XY &c = XY(), double ue = double(), double ns = double()) :
-            Edge(s, d, c1, c2, v), original_edge(-1), cross_type(a), original_point(b), cross_point(c), us_end_pos(ue), next_start_pos(ns) {}
+        ExpandMetaData(const Edge &e, int o=-1, Edge::EExpandCPType a = Edge::CP_TRIVIAL, const XY &c = XY(), double ue=double(), double ns=double()) :
+            Edge(e), original_edge(o), cross_type(a), cross_point(c), us_end_pos(ue), next_start_pos(ns) {}
+        ExpandMetaData(const XY&s, const XY &d, bool v = true, Edge::EExpandCPType a = Edge::CP_TRIVIAL, const XY &c = XY(), double ue = double(), double ns = double()) :
+            Edge(s, d, v), original_edge(-1), cross_type(a), cross_point(c), us_end_pos(ue), next_start_pos(ns) {}
+        ExpandMetaData(const XY&s, const XY &d, const XY&c1, const XY &c2, bool v = true, Edge::EExpandCPType a = Edge::CP_TRIVIAL, const XY &c = XY(), double ue = double(), double ns = double()) :
+            Edge(s, d, c1, c2, v), original_edge(-1), cross_type(a),cross_point(c), us_end_pos(ue), next_start_pos(ns) {}
     };
 
 protected:
